37394f099c359111441bcd5631a31969b600a029
samturton2 <STurton@spartaglobal.com>
2020-11-04 12:12:57 +0000

    Read me updated with best practises

--- README.md
+++ README.md
@@ -6,3 +6,23 @@
 
 - Functions will help us implement DRY
 
+- `return` key-word means the function will output that value when printed
+- some examples
+```python
+def subtract(num1, num2):
+# creating a subtract function that takes 2 args
+    print(num1 - num2)
+
+subtract(3, 2)
+
+def remainder(num1, num2):
+# returns the remainder when num1 is divided by num2
+    return num1 % num2
+
+print(remainder(7, 4))
+```
+# Best Practise
+- Best practise is to have a small block of code in any function that does one job.
+- Pseudo coding - one line of explanation
+- HINTs: create hints in simple bullet points or pointers
+- comments regarding you function results
--- functions.py
+++ functions.py
@@ -1,45 +1,47 @@
-# Let's create a greeting function
-
-# Syntax: def name_of_function():
-
-def greeting():
-    print("welcome on board")
-
-# if we execute this program now it would display nothing as we have not called this function
-
-# syntax to call the function
-greeting()
-# this will execute code inside function
-
-# create a new function that takes 2 arguments as ints and adds the value fo the to args
-
-def add(num1, num2):
-# creating an add function that takes 2 arguments
-    print(num1 + num2)
-# Displaying the sum of the args recieved
-
-add(4, 5)
-
-def subtract(num1, num2):
-# creating a subtract function that takes 2 args
-    print(num1 - num2)
-
-subtract(3, 2)
-
-def multiply(num1, num2):
-#creating a multiply function that takes 2 args
-    return num1 * num2
-
-print(multiply(4, 3))
-
-def divide(num1, num2):
-# creating a divide function that takes 2 args
-    return num1 / num2
-
-print(divide(12, 2))
-
-def remainder(num1, num2):
-# returns the remainder when num1 is divided by num2
-    return num1 % num2
-
-print(remainder(7, 4))
+# # Let's create a greeting function
+#
+# # Syntax: def name_of_function():
+#
+# def greeting():
+#     print("welcome on board")
+#
+# # if we execute this program now it would display nothing as we have not called this function
+#
+# # syntax to call the function
+# greeting()
+# # this will execute code inside function
+#
+# # create a new function that takes 2 arguments as ints and adds the value fo the to args
+#
+# def add(num1, num2):
+# # creating an add function that takes 2 arguments
+#     print(num1 + num2)
+# # Displaying the sum of the args recieved
+#
+# add(4, 5)
+#
+# def subtract(num1, num2):
+# # creating a subtract function that takes 2 args
+#     print(num1 - num2)
+#
+# subtract(3, 2)
+#
+# def multiply(num1, num2):
+# #creating a multiply function that takes 2 args
+#     return num1 * num2
+#
+# print(multiply(4, 3))
+#
+# def divide(num1, num2):
+# # creating a divide function that takes 2 args
+#     return num1 / num2
+#
+# print(divide(12, 2))
+#
+# def remainder(num1, num2):
+# # returns the remainder when num1 is divided by num2
+#     return num1 % num2
+#
+# print(remainder(7, 4))
+#
+# # We use the return statement which is the key to python functions which means return the result of any function
