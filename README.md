4ef1d538d8de497a2857a54967bdc006f234c30d
samturton2 <STurton@spartaglobal.com>
2020-11-04 11:46:09 +0000

    more maths functions created

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 ## How to create a function
 ## Functions and there abilities
 
-** DRY - DONT REPEAT YOURSELF **
+**DRY - DONT REPEAT YOURSELF**
 
 - Functions will help us implement DRY
 
--- functions.py
+++ functions.py
@@ -9,3 +9,37 @@ def greeting():
 
 # syntax to call the function
 greeting()
+# this will execute code inside function
+
+# create a new function that takes 2 arguments as ints and adds the value fo the to args
+
+def add(num1, num2):
+# creating an add function that takes 2 arguments
+    print(num1 + num2)
+# Displaying the sum of the args recieved
+
+add(4, 5)
+
+def subtract(num1, num2):
+# creating a subtract function that takes 2 args
+    print(num1 - num2)
+
+subtract(3, 2)
+
+def multiply(num1, num2):
+#creating a multiply function that takes 2 args
+    return num1 * num2
+
+print(multiply(4, 3))
+
+def divide(num1, num2):
+# creating a divide function that takes 2 args
+    return num1 / num2
+
+print(divide(12, 2))
+
+def remainder(num1, num2):
+# returns the remainder when num1 is divided by num2
+    return num1 % num2
+
+print(remainder(7, 4))
