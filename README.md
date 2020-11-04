66b73b8716a33f6da05275ccf2140f40f186f8ae
samturton2 <STurton@spartaglobal.com>
2020-11-04 11:13:54 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Functions
+## How to create a function
+## Functions and there abilities
+
+** DRY - DONT REPEAT YOURSELF **
+
+- Functions will help us implement DRY
+
---
+++ functions.py
@@ -0,0 +1,11 @@
+# Let's create a greeting function
+
+# Syntax: def name_of_function():
+
+def greeting():
+    print("welcome on board")
+
+# if we execute this program now it would display nothing as we have not called this function
+
+# syntax to call the function
+greeting()
